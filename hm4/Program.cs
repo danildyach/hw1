4cb3e65e09b0544e891f173d32cfcdded1ca9b95
Ivan Ivanov <you@example.com>
2023-06-22 23:10:00 +0300

    HomeWork4

---
+++ hm4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 8: Напишите программу, которая на вход принимает число(N), а на выходе показывает все чётные числа от 1 до N.
+            //5-> 2, 4
+            //8-> 2, 4, 6, 8
+
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= firstNumber; i++) 
+            {
+                if (i % 2 == 0) 
+                {
+                    
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+}

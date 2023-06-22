79d16f1351092dec8124de53728c58042848a848
Ivan Ivanov <you@example.com>
2023-06-22 23:03:59 +0300

    HomeWork3

---
+++ hd3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным(делится ли оно на два без остатка).
+            //4->да
+            //- 3->нет
+            //7->нет
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            if (firstNumber % 2 == 0)
+            {
+                Console.WriteLine($"Число {firstNumber} является четным ");
+            }
+            else 
+            {
+                Console.WriteLine($"Число {firstNumber} НЕ является четным ");
+            }
+        }
+
+        }
+    }
